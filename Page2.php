5482d53bdbff566b4c85977a8c6eef35ed6eae0c
Efrén Jimenez <EfJim_@hotmail.com>
2021-12-02 18:06:02 -0700

    EfJim V

---
+++ Page2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+    <html>
+
+        <head>
+
+            <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <link rel="stylesheet" href="mainstyle.css"/>
+                            <title>Prueba</title>
+
+        <!--Header-->
+        </head>
+
+            <!--Cuerpo de la página-->
+            <body>
+                
+                <header id="header">
+                    <!--Margen de la barra de navegación-->
+                    <div id="wrap">
+
+                        <div id="logo">
+                            <a href="#"></a>
+                        </div>
+
+                        <!--Barra de navegación-->
+                        <nav id="navbar">
+                     
+                        <!--Elementos de la barra de navegación-->
+                            <ul>
+                             <li><a href="#">Crear Receta</a></li>
+                             <li><a href="#">Regresar</a></li>
+                          </ul>
+                          <!--Fin de los Elementos de la barra de navegación-->
+                     </nav>
+                        <!--Fin de la Barra de navegación-->
+
+                        </div>
+                 <!--Fin del margen de la barra de navegación-->
+
+                </header>
+                <!--Fin del header-->
+
+                <!--Side Bar-->
+                <aside id="sidebar">
+                    <form id="form">
+                        <div id="cont">
+                            <input type="text" id="filtro"> <button id="btn">Buscar</button>
+                        </div>
+                        <button id="mts">Mostrar Todos</button>
+
+                        <select name=""> 
+                            <option class="select" selected disabled>País</option>
+                        </select>
+
+                        <select name="">
+                            <option class="select" selected disabled>Tipo</option>
+                        </select>
+
+                        <hr/>
+                      
+                    </form>
+                </aside>
+                <!--Fin de Side Bar-->
+
+                <!--Pie de página-->
+                <footer id="footer">
+                    <p>Developed By</p>
+                </footer>
+                <!--Fin del Pie de página-->
+
+            </body>
+
+    </html>
